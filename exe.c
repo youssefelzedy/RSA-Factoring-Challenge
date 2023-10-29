9783b9bb7c8b84617cd6113cb6a4b7aa21dcd8c5
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-10-29 22:44:26 +0200

    Adding File

--- exe.c
+++ exe.c
@@ -15,11 +15,16 @@ void exe(char *content, unsigned int counter, FILE *file)
 	(void)file;
 	(void)counter;
 
+	while(content[i] != '\n')
+	{
+		i++;
+	}
+	content[i] = '\0';
 	for(i = 2; i < num; i++)
 	{
 		if (num % i == 0)
 		{
-			printf("%ld=%ld*%d\n", num, num, 1);
+			printf("%s=%s*%d\n", content, content, 1);
 			break;
 		}
 	}
